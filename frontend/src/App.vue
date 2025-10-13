84ef23e7ca7a55a3f093c9ff990bd635b1d96a54
Fahadbcd17 <fahadbcd17@gmail.com>
2025-10-13 13:18:00 +0800

    core routing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <component :is="layout || 'div'">
+    <router-view :key="page" />
+  </component>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import { computed, defineAsyncComponent } from "vue";
+import { RouterView, useRoute } from "vue-router";
+
+let route = useRoute();
+
+const BaseLayoutComponent = defineAsyncComponent(() =>
+  import("./views/layout/BaseLayout.vue")
+);
+const UserLayoutComponent = defineAsyncComponent(() =>
+  import("./views/layout/UserLayout.vue")
+);
+
+const layout = computed(() => {
+  if (!route.meta.layout) {
+    return null; // No layout for this route
+  }
+  if (route.meta.layout === "UserLayout") {
+    return UserLayoutComponent;
+  }  else {
+    return BaseLayoutComponent;
+  }
+});
+
+const page = computed(() => route.name || "");
+</script>
+
+<style scoped></style>
