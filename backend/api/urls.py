6662b84437b3f5f57c25c0b27e023f3f1afda8b7
Fahadbcd17 <fahadbcd17@gmail.com>
2025-10-13 13:24:43 +0800

    give a endpoints in api/url

---
+++ backend/api/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, include, re_path
+from . import views
+from rest_framework import routers
+from .views import *
+router = routers.DefaultRouter()
+router.register(r'club', views.ClubViewSet, basename='club')
+router.register(r'post', views.PostViewSet, basename='post')
+router.register(r'presidents', PresidentViewSet)
+router.register(r'vice-presidents', VisePresidentViewSet)
+router.register(r'general-secretaries', GeneralSecretaryViewSet)
+router.register(r'treasurers', TreasurerViewSet)
+router.register(r'executives', ExecutivesViewSet)
+router.register(r'events', EventsViewSet)
+
+app_name = "api"
+urlpatterns =  [
+    re_path(r'^auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    re_path(r'^auth/', include('djoser.urls.jwt')),  
+]
+
+
+urlpatterns += router.urls
