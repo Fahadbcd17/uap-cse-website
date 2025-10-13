c7695189c00690cdf9751b70ad037615970dbc42
Fahadbcd17 <fahadbcd17@gmail.com>
2025-10-13 13:24:53 +0800

    register in admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register([Post,Club,Convener,President,VisePresident,GeneralSecretary,Treasurer,Executives,Events])
