fe67e401896accb91b6c94ce07fb3c8d1cd21fdf
Fahadbcd17 <fahadbcd17@gmail.com>
2025-10-17 19:45:43 +0800

    club registration added

--- frontend/src/views/user/ClubDetails.vue
+++ frontend/src/views/user/ClubDetails.vue
@@ -277,9 +277,9 @@
                 <span class="truncate">{{ event.location || 'TBA' }}</span>
               </div>
             </div>
-            <button class="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition-colors duration-200 text-sm font-semibold flex-shrink-0">
+            <a href="https://docs.google.com/forms/d/1f8FHNNlEZSTRfi4h-AYgrnA0f2VoEOPeWh-C6XtRmS0/edit" target="_blank" class="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition-colors duration-200 text-sm font-semibold flex-shrink-0">
               Register
-            </button>
+            </a>
           </div>
         </div>
       </div>
